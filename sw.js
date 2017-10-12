560ca7dca59b298fc57f0fd7275e42b533689743
Евгений Ворожцов <evorozhtsov@htc-cs.com>
2017-10-12 12:01:37 +0400

    change path to json

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('sync', e => {
     if (e.tag == 'syncdata') {
-        fetch('/data.json', { method: 'post'}).then(
+        fetch('data.json', { method: 'get'}).then(
             (req) => {
                 if (req.status === 200) {
                     self.registration.showNotification('Data sync');
